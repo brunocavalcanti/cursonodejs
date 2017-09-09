51832b35961a68d7f589e5ad0c204ab979d009e1
Aluno <aluno@LIPENET.local>
2017-09-09 11:53:52 -0300

    primeiro script com nodejs

---
+++ aula01/api/script.js
@@ -0,0 +1,12 @@
+var frutas = ["banana","caqui","caju"]
+
+for(i in frutas){
+    console.log('FRUTA :', frutas[i])
+}
+setTimeout(function(){
+    console.log("outra msg")
+},2000)
+
+setTimeout(function(){
+    console.log("outra msg marota")
+},1000)
